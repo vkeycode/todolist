35bc31639e9baae185dcfb46b3018d1cfa95a51e
Vedat Eroglu <vdterog@gmail.com>
2022-06-27 18:18:52 +0300

    Added comment to javascript file

--- script/scripts.js
+++ script/scripts.js
@@ -1,37 +1,37 @@
 let formDOM = document.querySelector('#userForm')
 
-formDOM.addEventListener('submit', formSubmit)
+formDOM.addEventListener('submit', formSubmit) // form DOM after click formSubmit event run
 
 let ulDOM = document.querySelector('#list')
 
 
-function formSubmit(event){
+function formSubmit(event){ // form submit event
         
-        event.preventDefault()
+        event.preventDefault() //defaul none so no refresh page
         
-        let formText = document.querySelector('#Notes')
-        localStorage.setItem('notes', formText.value)
+        let formText = document.querySelector('#Notes') 
+        localStorage.setItem('notes', formText.value)  // set local storage value
         if(formText.value == ''){
-                alert("Lütfen Bir Yapılacak Durum Giriniz")
+                alert("Lütfen Bir Yapılacak Durum Giriniz") // if formText.value is empty
         }
         else{
-        formText.value = ''
-        let newLi = document.createElement('li')
-        newLi.setAttribute('id', 'addText')
-        newLi.classList.add('p-2','mb-2', 'rounded', 'd-flex', 'justify-content-between')
-        newLi.style.color = 'black'
-        newLi.style.fontWeight = 'bold'
-        newLi.style.backgroundColor = 'gray'
+        formText.value = '' // delete formText.value
+        let newLi = document.createElement('li') // createElement newli
+        newLi.setAttribute('id', 'addText') // add id to element
+        newLi.classList.add('p-2','mb-2', 'rounded', 'd-flex', 'justify-content-between') // add class to element
+        newLi.style.color = 'black'// add style to element
+        newLi.style.fontWeight = 'bold' // add style to element
+        newLi.style.backgroundColor = 'gray' // add style to element
         
-        ulDOM.append(newLi)
-        newLi.innerHTML = localStorage.getItem('notes')
-        newLi.innerHTML += `<i class="fa-solid fa-xmark" id="removeButton" style=" cursor: pointer" onClick="removeTask(this)"></i>`
+        ulDOM.append(newLi) // add element in ulDOM
+        newLi.innerHTML = localStorage.getItem('notes') // get localStorage value  in newli innerHTML
+        newLi.innerHTML += `<i class="fa-solid fa-xmark" id="removeButton" style=" cursor: pointer" onClick="removeTask(this)"></i>` // add delete button in newli innerHTML and after onclick run function removeTask;
         }
         
 }
 
 function removeTask(e){
-        e.parentNode.parentNode.removeChild(e.parentNode);
+        e.parentNode.parentNode.removeChild(e.parentNode); // removeTask is call 
 }
 
 
